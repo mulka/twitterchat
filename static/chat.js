4393870ba3a03ab7b3ccf520e384bae2d9d702b8
Kyle Mulka <repalviglator@yahoo.com>
2013-06-08 19:36:45 -0400

    removed Javascript code for new message stuff

--- static/chat.js
+++ static/chat.js
@@ -1,18 +1,6 @@
 $(document).ready(function() {
     if (!window.console) window.console = {};
     if (!window.console.log) window.console.log = function() {};
-
-    $("#messageform").live("submit", function() {
-        newMessage($(this));
-        return false;
-    });
-    $("#messageform").live("keypress", function(e) {
-        if (e.keyCode == 13) {
-            newMessage($(this));
-            return false;
-        }
-    });
-    $("#message").select();
     updater.poll();
 });
 
